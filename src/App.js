66a0992b72b333a0c0207e7d4a969190a56df095
Abhishek <ak724591@gmail.com>
2021-11-24 20:24:32 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import Galleries from './Galleries'
+
+const App = () => {
+  const [items, setitems] = useState(Galleries)
+
+  const filteritems = (sitems) => {
+    const updateditems = Galleries.filter((celms) => {
+      return celms.name.toLowerCase() === sitems.toLowerCase()
+    })
+
+    if (sitems === "") {
+      setitems(Galleries)
+    } else {
+      setitems(updateditems)
+    }
+  }
+  return (
+    <>
+      <div className="mdiv">
+        <div className="hdiv">
+          <p>Sound Clode</p>
+          <div className="searchbar">
+            <input className="inpstyle" type="text" placeholder="Search songs" onChange={(e) => filteritems(e.target.value)} />
+          </div>
+        </div>
+        <div className="cdiv">
+          <div className="cards">
+            {
+              items.map((elem) => {
+                return (
+                  <div className="mcards">
+                    <img src={elem.image} alt="" />
+                    <h6>{elem.name}</h6>
+                    <p>{elem.artist}</p>
+                    <h6>{elem.time} &nbsp; {elem.likes}</h6>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+        <div className="fdiv">
+          <div className="line"></div>
+          <div className="sdetail">
+            <div>01:30 / 03:30 &nbsp; Max</div>
+            <div>1.0</div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Galleries.js
@@ -0,0 +1,60 @@
+const Galleries = [
+    {
+        image: "https://source.unsplash.com/150x150?martingarrix",
+        name: "Animal",
+        artist: "Martin Garrix",
+        time: "05:24",
+        likes: "4,73,498"
+    },
+    {
+        image: "https://source.unsplash.com/150x150?dj",
+        name: "Levels",
+        artist: "Avici",
+        time: "07:64",
+        likes: "6,73,498"
+    },
+    {
+        image: "https://source.unsplash.com/150x150?music",
+        name: "Sacrifice",
+        artist: "Bebe Rexa",
+        time: "02:64",
+        likes: "73,498"
+    },
+    {
+        image: "https://source.unsplash.com/150x150?piano",
+        name: "Stay",
+        artist: "Justin Biber",
+        time: "07:64",
+        likes: "7,73,498"
+    },
+    {
+        image: "https://source.unsplash.com/150x150?edm",
+        name: "Baby",
+        artist: "Justin Biber",
+        time: "05:64",
+        likes: "3,73,498"
+    },
+    {
+        image: "https://source.unsplash.com/150x150?earphone",
+        name: "Follow",
+        artist: "Justin Biber",
+        time: "04:64",
+        likes: "73,498"
+    },
+    {
+        image: "https://source.unsplash.com/150x150?smartphone",
+        name: "Ghost",
+        artist: "Justin Biber",
+        time: "05:64",
+        likes: ".2,73,498"
+    },
+    {
+        image: "https://source.unsplash.com/150x150?dvd",
+        name: "Yammy",
+        artist: "Justin Biber",
+        time: "05:64",
+        likes: ".2,73,498"
+    },
+]
+
+export default Galleries;
